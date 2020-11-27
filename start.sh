e4c5f6f0930fabf05caeb896887939ae3d9f7678
liveuak <liveuak@gmail.com>
2020-11-27 19:49:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@
 if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	echo "Rclone config detected"
 	echo -e "[DRIVE]\n$RCLONE_CONFIG" > rclone.conf
-	echo "on-download-stop=./delete.sh" >> aria2c.conf
-	echo "on-download-complete=./on-complete.sh" >> aria2c.conf
+	echo "on-download-stop=./delete.sh" >> airc.conf
+	echo "on-download-complete=./on-complete.sh" >> airc.conf
 	chmod +x delete.sh
 	chmod +x on-complete.sh
 fi
 
-echo "rpc-secret=$ARIA2C_SECRET" >> aria2c.conf
-aria2c --conf-path=aria2c.conf&
+echo "rpc-secret=$ARIA2C_SECRET" >> airc.conf
+aria2c --conf-path=airc.conf&
 yarn start
