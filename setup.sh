222823f34ae422f011f0ff2fa92097e569a3ecd1
liveuak <liveuak@gmail.com>
2020-11-27 19:50:17 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,4 +19,4 @@ wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
 
 # Tracker
 tracker_list=`curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat`
-echo "bt-tracker=$tracker_list" >> aric.conf
+echo "bt-tracker=$tracker_list" >> airc.conf
